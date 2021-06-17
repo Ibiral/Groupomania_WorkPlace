09988b108b9e47b6ff90f64d9e829156a0536f1f
Ibiral <tnt_djurdjura@hotmail.fr>
2021-06-17 10:28:08 +0200

    changement du nom de la base de données

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 module.exports = {
     "development": {
-      "username": "root",
+      "username": "root", 
       "password": "",
-      "database": "groupomania_db",
+      "database": "groupomania_database",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
